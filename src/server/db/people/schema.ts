b8c93945b692d1d32e0f17a0ddac575d7c801b99
giannibuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-05-08 01:42:56 -0400

    Final Schema Rewrite?

---
+++ src/server/db/people/schema.ts
@@ -0,0 +1,124 @@
+import { relations } from "drizzle-orm";
+import { integer, real, text } from "drizzle-orm/sqlite-core";
+import { createTable } from "../createTable";
+import { titles } from "../schemaIndex";
+import { createId } from "@paralleldrive/cuid2";
+
+// AGENCIES
+export const agencies = createTable("agencies", {
+  id: integer("id").notNull().primaryKey({ autoIncrement: true }),
+  name: text("name", { length: 255 }).notNull(),
+  website: text("website", { length: 255 }).unique(),
+  address: text("address", { length: 255 }),
+  tel: integer("tel"),
+  flagged: integer("flagged", { mode: "boolean" }).notNull().default(false),
+});
+
+export const agencyRelations = relations(agencies, ({ many }) => ({
+  agents: many(agents),
+}));
+
+export const agents = createTable("agents", {
+  id: integer("id").notNull().primaryKey({ autoIncrement: true }),
+  firstName: text("first_name", { length: 50 }),
+  lastName: text("last_name", { length: 50 }),
+  email: text("email", { length: 255 }).unique(),
+  tel: integer("tel"),
+  haveWorkedWith: integer("have_worked_with", { mode: "boolean" })
+    .notNull()
+    .default(false),
+  agencyId: text("agency_id", { length: 10 }).references(() => agencies.id),
+});
+
+export const agentRelations = relations(agents, ({ one, many }) => ({
+  agency: one(agencies, {
+    fields: [agents.agencyId],
+    references: [agencies.id],
+  }),
+  illustrators: many(illos),
+  authors: many(authors),
+}));
+
+// AUTHORS
+export const authors = createTable("authors", {
+  id: integer("id").notNull().primaryKey({ autoIncrement: true }),
+  firstName: text("first_name", { length: 50 }),
+  lastName: text("last_name", { length: 50 }),
+  email: text("email", { length: 255 }).unique(),
+  tel: integer("tel"),
+  address: text("address", { length: 255 }),
+  haveWorkedWith: integer("have_worked_with", { mode: "boolean" })
+    .notNull()
+    .default(false),
+  lastAdvance: real("last_advance"),
+  agentId: text("agent_id", { length: 10 }).references(() => agents.id),
+});
+
+export const authorRelations = relations(authors, ({ one, many }) => ({
+  agent: one(agents, {
+    fields: [authors.agentId],
+    references: [agents.id],
+  }),
+  developments: many(developments),
+}));
+
+// ILLOS
+export const illos = createTable("illos", {
+  id: integer("id").notNull().primaryKey({ autoIncrement: true }),
+  firstName: text("first_name", { length: 50 }),
+  lastName: text("last_name", { length: 50 }),
+  email: text("email", { length: 255 }).unique(),
+  tel: integer("tel"),
+  address: text("address", { length: 255 }),
+  haveWorkedWith: integer("have_worked_with", { mode: "boolean" })
+    .notNull()
+    .default(false),
+  lastAdvance: real("last_advance"),
+  agentId: text("agent_id", { length: 10 }).references(() => agents.id),
+});
+
+export const illoRelations = relations(illos, ({ one, many }) => ({
+  agent: one(agents, {
+    fields: [illos.agentId],
+    references: [agents.id],
+  }),
+  developments: many(developments),
+}));
+
+//EDITORS
+export const editors = createTable("editors", {
+  id: integer("id").notNull().primaryKey({ autoIncrement: true }),
+  firstName: text("first_name", { length: 50 }),
+  lastName: text("last_name", { length: 50 }),
+  email: text("email", { length: 255 }).unique(),
+});
+
+//DEVELOPMENTS
+export const developments = createTable("developments", {
+  id: text("id", { length: 24 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  authorId: text("author_id", { length: 10 }).references(() => authors.id),
+  editorId: text("editor_id", { length: 10 }).references(() => editors.id),
+  illoId: text("illo_id", { length: 10 }).references(() => illos.id),
+});
+
+export const developmentRelations = relations(
+  developments,
+  ({ one, many }) => ({
+    author: one(authors, {
+      fields: [developments.authorId],
+      references: [authors.id],
+    }),
+    editor: one(editors, {
+      fields: [developments.editorId],
+      references: [editors.id],
+    }),
+    illo: one(illos, {
+      fields: [developments.illoId],
+      references: [illos.id],
+    }),
+    titles: many(titles),
+  }),
+);
--- src/server/db/titles/schema.ts
+++ src/server/db/titles/schema.ts
@@ -2,191 +2,87 @@ import { createTable } from "@/server/db/createTable";
 import { createId } from "@paralleldrive/cuid2";
 import { relations } from "drizzle-orm";
 import { integer, real, text } from "drizzle-orm/sqlite-core";
+import { developments } from "../people/schema";
 
-//TITLES
 export const titles = createTable("titles", {
-  id: text("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  title: text("title", { length: 255 }).notNull(),
+  workRef: integer("work_ref").notNull().primaryKey(),
+  title: text("title", { length: 255 }),
   subtitle: text("subtitle", { length: 255 }),
-  isbn: integer("isbn", { mode: "number" }),
-  width: real("width"),
-  height: real("height"),
+  isbn: integer("isbn").unique(),
   price: real("price"),
-  priority: text("priority", { enum: ["high", "low", "normal"] }),
-  note: text("note", { length: 255 }),
-  skedId: text("sked_id", { length: 255 }).references(() => titleSked.id),
-  withWhomst: text("with_whomst", {
-    enum: ["agent", "author", "editor", "illo", "me"],
-  }),
+  width: integer("width"),
+  height: integer("height"),
+  developmentId: text("developmentId", { length: 10 }).references(
+    () => developments.id,
+  ),
 });
 
-//ONE TITLE TO ONE SKED
-export const titleRelations = relations(titles, ({ one }) => ({
-  sked: one(titleSked, {
-    fields: [titles.skedId],
-    references: [titleSked.id],
+export const titleRelations = relations(titles, ({ one, many }) => ({
+  development: one(developments, {
+    fields: [titles.developmentId],
+    references: [developments.id],
   }),
+  skeds: many(titleSkeds),
 }));
 
-//TITLE SKEDS
-export const titleSked = createTable("title_schedule", {
-  id: text("id", { length: 255 })
+export const titleSkeds = createTable("title_skeds", {
+  id: text("id", { length: 24 })
     .notNull()
     .primaryKey()
     .$defaultFn(() => createId()),
-  osd: text("osd"),
+  osd: text("osd", { length: 10 }),
+  titleWorkRef: integer("title_work_ref").references(() => titles.workRef),
 });
 
-//ONE SKED TO MANY STAGES
-export const scheduleRelations = relations(titleSked, ({ many }) => ({
-  stages: many(skedStage),
+export const skedRelations = relations(titleSkeds, ({ one, many }) => ({
+  title: one(titles, {
+    fields: [titleSkeds.titleWorkRef],
+    references: [titles.workRef],
+  }),
+  stages: many(skedStages),
 }));
 
-//SKED STAGE
-export const skedStage = createTable("sked_stage", {
-  id: integer("id").notNull().primaryKey({ autoIncrement: true }),
+export const skedStages = createTable("sked_stages", {
+  id: text("id", { length: 10 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => createId()),
   name: text("name", {
     enum: [
-      "Text to Design",
-      "Illo Signed Up",
-      "GSP",
+      "Text To Designer",
+      "Illo Sign Up GSP",
       "Sketches In",
-      "Sketches Approved",
+      "Sketches Sign Off",
+      "Launch Materials",
       "Art In",
+      "Conference Materials",
+      "Art Sign Off",
       "Forecast 1",
-      "Art Approved",
       "Proofreading",
-      "Specs Finalized",
-      "Design Editorial Sign-off",
-      "Production Sign-off",
-      "Files to Pinter",
+      "Final Sign Off",
+      "Files To Printer",
+      "Intermediate Materials",
       "Proofs In",
       "Proofs Approved",
-      "Advanced In",
-      "Advanced Approved",
-      "Archiving",
-      "Launch",
-      "Conference",
-      "Intermediate",
       "Final Materials",
+      "Advances In",
+      "Archiving",
+      "On Sale Date",
     ],
-    length: 255,
   }).notNull(),
-  due: text("due"),
+  date: text("date", { length: 10 }),
   done: integer("done", { mode: "boolean" }).notNull().default(false),
   loggedInBiblio: integer("logged_in_biblio", { mode: "boolean" })
     .notNull()
     .default(false),
-  skedId: text("sked_id").references(() => titleSked.id),
+  titleSkedId: text("title_sked_id", { length: 24 }).references(
+    () => titleSkeds.id,
+  ),
 });
 
-//EACH STAGE ROW BELONGS TO ONLY ONE SKED
-export const skedStageRelations = relations(skedStage, ({ one }) => ({
-  sked: one(titleSked, {
-    fields: [skedStage.skedId],
-    references: [titleSked.id],
+export const skedStageRelations = relations(skedStages, ({ one }) => ({
+  titleSked: one(titleSkeds, {
+    fields: [skedStages.titleSkedId],
+    references: [titleSkeds.id],
   }),
 }));
-
-// EDITORS
-export const editors = createTable("editors", {
-  id: text("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  name: text("name", { length: 255 }),
-  email: text("email", { length: 255 }).notNull().unique(),
-});
-
-// TEAM & TITLES JOIN TABLE
-export const teamOnTitles = createTable("team_on_titles", {
-  id: text("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  titleId: text("title_id", { length: 255 }).references(() => titles.id),
-  editorId: text("editor_id", { length: 255 }).references(() => editors.id),
-  creatorId: text("creator_id", { length: 255 }).references(() => creators.id),
-});
-
-//CREATORS
-export const creators = createTable("creators", {
-  id: text("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  name: text("name", { length: 255 }).notNull(),
-  role: text("role", { enum: ["illustrator", "author"] }),
-  email: text("email", { length: 255 }).unique(),
-  background: text("background", { length: 255 }),
-  haveWorkedWith: integer("have_worked_with", { mode: "boolean" })
-    .notNull()
-    .default(false),
-  pronouns: text("pronouns", { length: 255 }),
-  website: text("website", { length: 255 }),
-  address: text("address", { length: 255 }),
-  tel: integer("tel"),
-  agentId: text("agent_id", { length: 255 }).references(() => agents.id),
-});
-
-//CREATOR RELATIONS
-export const creatorRelations = relations(creators, ({ one }) => ({
-  agents: one(agents, {
-    fields: [creators.agentId],
-    references: [agents.id],
-  }),
-}));
-
-// EDITORS ON TITLES RELATIONS
-export const editorsOnTitlesRelations = relations(teamOnTitles, ({ one }) => ({
-  titles: one(titles, {
-    fields: [teamOnTitles.titleId],
-    references: [titles.id],
-  }),
-  editors: one(editors, {
-    fields: [teamOnTitles.editorId],
-    references: [editors.id],
-  }),
-  creators: one(creators, {
-    fields: [teamOnTitles.creatorId],
-    references: [creators.id],
-  }),
-}));
-
-//AGENT
-export const agents = createTable("agents", {
-  id: text("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  name: text("name", { length: 255 }).notNull(),
-  email: text("email", { length: 255 }).unique(),
-  haveWorkedWith: integer("have_worked_with", { mode: "boolean" })
-    .notNull()
-    .default(false),
-  agencyId: text("agency_id", { length: 255 }).references(() => agencies.id),
-});
-
-// AGENT RELATIONS
-export const agentRelations = relations(agents, ({ one, many }) => ({
-  creators: many(creators),
-  agencies: one(agencies, {
-    fields: [agents.agencyId],
-    references: [agencies.id],
-  }),
-}));
-
-//AGENCY
-export const agencies = createTable("agencies", {
-  id: text("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  name: text("name", { length: 255 }).notNull(),
-  website: text("website", { length: 255 }),
-  address: text("address", { length: 255 }),
-  tel: integer("tel"),
-});
